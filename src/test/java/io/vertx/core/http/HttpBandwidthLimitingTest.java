0821c817187cdd78f74c6ef55875fbef3820a2dc
Saranya Krishnakumar <saran.krishna920@gmail.com>
2021-11-11 11:16:11 -0800

    Add GlobalTrafficShapingHandler to server pipeline for bandwidth limiting (#7) Co-authored-by: "Dinesh A. Joshi" <dinesh.joshi@apple.com>

---
+++ src/test/java/io/vertx/core/http/HttpBandwidthLimitingTest.java
@@ -0,0 +1,284 @@
+/*
+ * Copyright (c) 2011-2021 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.core.http;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.net.TrafficShapingOptions;
+import org.apache.commons.lang.time.StopWatch;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.test.core.TestUtils;
+
+@RunWith(Parameterized.class)
+public class HttpBandwidthLimitingTest extends Http2TestBase {
+  private static final int OUTBOUND_LIMIT = 2 * 64 * 1024;  // 64KB/s
+  private static final int INBOUND_LIMIT = 64 * 1024;   // 64KB/s
+  private static final int TEST_CONTENT_SIZE = 64 * 1024 * 4;   // 64 * 4 = 256KB
+
+  private final File sampleF = new File(new File(TestUtils.MAVEN_TARGET_DIR, "test-classes"), "test_traffic.txt");
+  private final Handlers HANDLERS = new Handlers();
+
+  @Parameters(name = "HTTP {0}")
+  public static Iterable<Object[]> data() {
+
+    Function<Vertx, HttpServer> http1ServerFactory = (v) -> Providers.http1Server(v, INBOUND_LIMIT, OUTBOUND_LIMIT);
+    Function<Vertx, HttpServer> http2ServerFactory = (v) -> Providers.http2Server(v, INBOUND_LIMIT, OUTBOUND_LIMIT);
+    Function<Vertx, HttpClient> http1ClientFactory = (v) -> v.createHttpClient();
+    Function<Vertx, HttpClient> http2ClientFactory = (v) -> v.createHttpClient(createHttp2ClientOptions());
+
+    return Arrays.asList(new Object[][] {
+      { 1.1, http1ServerFactory, http1ClientFactory },
+      { 2.0, http2ServerFactory, http2ClientFactory }
+    });
+  }
+
+  private Function<Vertx, HttpServer> serverFactory;
+  private Function<Vertx, HttpClient> clientFactory;
+
+  public HttpBandwidthLimitingTest(double protoVersion, Function<Vertx, HttpServer> serverFactory,
+                                   Function<Vertx, HttpClient> clientFactory) {
+    this.serverFactory = serverFactory;
+    this.clientFactory = clientFactory;
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    super.setUp();
+    server = serverFactory.apply(vertx);
+    client = clientFactory.apply(vertx);
+  }
+
+  @After
+  public void after() throws InterruptedException
+  {
+    CountDownLatch waitForClose = new CountDownLatch(1);
+    vertx.close().onComplete(resp -> waitForClose.countDown());
+    awaitLatch(waitForClose);
+  }
+
+  @Test
+  public void sendBufferThrottled() throws Exception {
+    Buffer expectedBuffer = TestUtils.randomBuffer(TEST_CONTENT_SIZE);
+
+    HttpServer testServer = serverFactory.apply(vertx);
+    StopWatch watch = new StopWatch();
+    testServer.requestHandler(HANDLERS.bufferRead(expectedBuffer));
+    startServer(testServer);
+
+    watch.start();
+    HttpClient testClient = clientFactory.apply(vertx);
+    read(expectedBuffer, testServer, testClient);
+    await();
+    watch.stop();
+
+    Assert.assertTrue(watch.getTime() > expectedTimeMillis(TEST_CONTENT_SIZE, OUTBOUND_LIMIT));
+  }
+
+  @Test
+  public void sendFileIsThrottled() throws Exception {
+    StopWatch watch = new StopWatch();
+    HttpServer testServer = serverFactory.apply(vertx);
+    testServer.requestHandler(HANDLERS.getFile(sampleF));
+    startServer(testServer);
+
+    watch.start();
+    HttpClient testClient = clientFactory.apply(vertx);
+    testClient.request(HttpMethod.GET, testServer.actualPort(), DEFAULT_HTTP_HOST,"/get-file")
+              .compose(HttpClientRequest::send)
+              .compose(HttpClientResponse::body)
+              .onComplete(body -> testComplete());
+    await();
+    watch.stop();
+
+    Assert.assertTrue(watch.getTime() > 1000);
+  }
+
+  @Test
+  public void dataUploadIsThrottled() throws Exception {
+    Buffer expectedBuffer = TestUtils.randomBuffer((TEST_CONTENT_SIZE));
+
+    StopWatch watch = new StopWatch();
+    HttpServer testServer = serverFactory.apply(vertx);
+    testServer.requestHandler(HANDLERS.bufferWrite(expectedBuffer));
+    startServer(testServer);
+
+    watch.start();
+    HttpClient testClient = clientFactory.apply(vertx);
+    write(expectedBuffer, testServer, testClient);
+    await();
+    watch.stop();
+
+    Assert.assertTrue(watch.getTime() > expectedTimeMillis(TEST_CONTENT_SIZE, INBOUND_LIMIT));
+  }
+
+  @Test
+  public void fileUploadIsThrottled() throws Exception {
+    StopWatch watch = new StopWatch();
+    HttpServer testServer = serverFactory.apply(vertx);
+    testServer.requestHandler(HANDLERS.uploadFile(sampleF));
+    startServer(testServer);
+
+    watch.start();
+    HttpClient testClient = clientFactory.apply(vertx);
+    upload(testServer, testClient, sampleF);
+    await();
+    watch.stop();
+
+    Assert.assertTrue( watch.getTime() > 1000);
+  }
+
+  @Test
+  public void testSendFileTrafficShapedWithSharedServers() throws InterruptedException {
+    int numEventLoops = 4; // We start a shared TCP server with 4 event-loops
+    Future<String> listenLatch = vertx.deployVerticle(() -> new AbstractVerticle() {
+      @Override
+      public void start(Promise<Void> startPromise) {
+        HttpServer testServer = serverFactory.apply(vertx);
+        testServer.requestHandler(HANDLERS.getFile(sampleF))
+                  .listen(DEFAULT_HTTP_PORT, DEFAULT_HTTP_HOST).<Void>mapEmpty().onComplete(startPromise);
+      }
+    }, new DeploymentOptions().setInstances(numEventLoops));
+
+    HttpClient testClient = clientFactory.apply(vertx);
+    StopWatch watch = new StopWatch();
+    CountDownLatch waitForResponse = new CountDownLatch(4);
+    listenLatch.onComplete(v -> {
+      watch.start();
+      for (int i=0; i<4; i++) {
+        testClient.request(HttpMethod.GET, DEFAULT_HTTP_PORT, DEFAULT_HTTP_HOST,"/get-file")
+                  .compose(HttpClientRequest::send)
+                  .compose(HttpClientResponse::body)
+                  .onComplete(body -> waitForResponse.countDown());
+      }
+    });
+    awaitLatch(waitForResponse);
+    watch.stop();
+    Assert.assertTrue(watch.getTime() > 6000); // because there are simultaneous 4 requests
+  }
+
+  /**
+   * The throttling takes a while to kick in so the expected time cannot be strict especially
+   * for small data sizes in these tests.
+   *
+   * @param size
+   * @param rate
+   * @return
+   */
+  private long expectedTimeMillis(int size, int rate) {
+    return (long) (TimeUnit.MILLISECONDS.convert((size / rate), TimeUnit.SECONDS) * 0.5);
+  }
+
+  private void read(Buffer expected, HttpServer server, HttpClient client) {
+    client.request(HttpMethod.GET, server.actualPort(), DEFAULT_HTTP_HOST,"/buffer-read")
+          .compose(req -> req.send()
+                             .compose(HttpClientResponse::body))
+          .onComplete(body -> {
+            assertEquals(expected.getByteBuf(), body.result().getByteBuf());
+            testComplete();
+          });
+  }
+
+  private void write(Buffer buffer, HttpServer server, HttpClient client) {
+    client.request(HttpMethod.POST, server.actualPort(), DEFAULT_HTTP_HOST, "/buffer-write")
+          .compose(req -> req.putHeader(HttpHeaderNames.CONTENT_LENGTH, String.valueOf(buffer.length()))
+                             .end(buffer));
+  }
+
+  private void upload(HttpServer server, HttpClient client, File expected) {
+    Buffer b = vertx.fileSystem().readFileBlocking(expected.getAbsolutePath());
+    client.request(HttpMethod.PUT, server.actualPort(), DEFAULT_HTTP_HOST, "/upload-file")
+          .compose(req -> req.putHeader(HttpHeaderNames.CONTENT_LENGTH, String.valueOf(expected.length()))
+                           .putHeader(HttpHeaderNames.CONTENT_TYPE, "application/binary")
+                           .end(b));
+  }
+
+  class Handlers {
+    public Handler<HttpServerRequest> bufferRead(Buffer expectedBuffer) {
+      return (req) -> {
+        req.response().setChunked(true);
+
+        int start = 0;
+        int size = expectedBuffer.length();
+        int chunkSize = OUTBOUND_LIMIT / 2;
+        while (size > 0) {
+          int len = Math.min(chunkSize, size);
+          req.response().write(expectedBuffer.getBuffer(start, start + len));
+          start += len;
+          size -= len;
+        }
+        req.response().end();
+      };
+    }
+
+    public Handler<HttpServerRequest> getFile(File expected) {
+      return req -> req.response().sendFile(expected.getAbsolutePath());
+    }
+
+    public Handler<HttpServerRequest> bufferWrite(Buffer expected) {
+      return req -> {
+        req.bodyHandler(buffer -> {
+          assertEquals(expected.getByteBuf(), buffer.getByteBuf());
+          testComplete();
+        });
+      };
+    }
+
+    public Handler<HttpServerRequest> uploadFile(File expected) {
+      return req -> {
+        req.endHandler((r) -> {
+          assertEquals(expected.length(), req.bytesRead());
+          testComplete();
+        });
+      };
+    }
+  }
+
+  static class Providers {
+    private static HttpServer http1Server(Vertx vertx, int inboundLimit, int outboundLimit) {
+      HttpServerOptions options = new HttpServerOptions()
+                                    .setHost(DEFAULT_HTTP_HOST)
+                                    .setPort(DEFAULT_HTTP_PORT)
+                                    .setTrafficShapingOptions(new TrafficShapingOptions()
+                                                                .setInboundGlobalBandwidth(inboundLimit)
+                                                                .setOutboundGlobalBandwidth(outboundLimit));
+
+      return vertx.createHttpServer(options);
+    }
+
+    private static HttpServer http2Server(Vertx vertx, int inboundLimit, int outboundLimit) {
+      HttpServerOptions options = createHttp2ServerOptions(DEFAULT_HTTP_PORT, DEFAULT_HTTP_HOST)
+                                    .setTrafficShapingOptions(new TrafficShapingOptions()
+                                                                .setInboundGlobalBandwidth(inboundLimit)
+                                                                .setOutboundGlobalBandwidth(outboundLimit));
+
+      return vertx.createHttpServer(options);
+    }
+  }
+}
